7c39dc7da5d8a0bf9a97be816dfdebfb09431f8c
Karmesh Patel <Karmesh827@gmail.com>
2022-12-11 13:23:14 -0500

    aoc2022 day11

---
+++ day11/day11part1.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+
+def populateDic():
+    with open('day11/inputDay11.txt') as f:
+        lines = f.readlines()
+        monkeyDictionary = defaultdict(dict)
+        monkeyCounter = 0
+        lineCounter = 0
+        for line in lines:
+            line1 = line.rstrip().split(' ')
+            if(len(line1) == 1):
+                monkeyCounter += 1
+                lineCounter = 0
+                continue
+            if(lineCounter == 0):
+                monkeyDictionary[monkeyCounter]['inspectionCount'] = 0
+            if(lineCounter == 1):
+                line2 = line.rstrip().split(':')
+                innerLine = line2[1].strip().split(', ')
+                monkeyDictionary[monkeyCounter]['items'] = innerLine
+            if(lineCounter == 2):
+                line3 = line.rstrip().split('= ')[1]
+                monkeyDictionary[monkeyCounter]['formula'] = line3
+            if(lineCounter == 3):
+                monkeyDictionary[monkeyCounter]['divisible'] = line.rstrip().split('by ')[1]
+            if(lineCounter == 4):
+                monkeyDictionary[monkeyCounter]['trueThrow'] = line.rstrip().split('monkey ')[1]
+            if(lineCounter == 5):
+                monkeyDictionary[monkeyCounter]['falseThrow'] = line.rstrip().split('monkey ')[1]
+            
+            lineCounter += 1
+
+        return monkeyDictionary
+
+
+def executeRound(monkeyDict):
+    for key in monkeyDict:
+        items = monkeyDict[key]['items']
+        for item in items:
+            monkeyDict[key]['inspectionCount'] += 1
+            old = int(item)
+            newWorryLevel = eval(monkeyDict[key]['formula'])
+            newWorryLevel = newWorryLevel//3
+            divisible = int(monkeyDict[key]['divisible'])
+            if(newWorryLevel % divisible == 0):
+                throwTo = int(monkeyDict[key]['trueThrow'])
+                monkeyDict[throwTo]['items'].append(str(newWorryLevel))
+            else:
+                throwTo = int(monkeyDict[key]['falseThrow'])
+                monkeyDict[throwTo]['items'].append(str(newWorryLevel))
+        monkeyDict[key]['items'] = []
+    return monkeyDict
+
+
+def part1Answer():
+    initialDict = populateDic()
+    monkeyInspectionCounter = []
+    maxVal = 0
+    secondMax = 0
+    for i in range(20):
+        initialDict = executeRound(initialDict)
+    for key in initialDict:
+        if initialDict[key]['inspectionCount'] > maxVal:
+            secondMax = maxVal
+            maxVal = initialDict[key]['inspectionCount']
+        elif initialDict[key]['inspectionCount'] > secondMax:
+            secondMax = initialDict[key]['inspectionCount']
+    print(maxVal*secondMax)
+
+part1Answer()
